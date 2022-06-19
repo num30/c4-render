601f8cea5c405f30823a83c0cf2817d0fd9493c3
Denis Palnitsky <palnitsky@gmail.com>
2022-06-19 14:43:49 +0200

    Add logging

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#docker run -it --rm -v $PWD:/usr/local/structurizr structurizr/cli export -f plantuml -w  $FILE_NAME -o rendered
 mkdir -p $TARGET_PATH
 docker run -it --rm -v $PWD:/usr/local/structurizr structurizr/cli export -f mermaid -w $FILE_NAME -o $TARGET_PATH/mermaid
 
@@ -14,11 +15,12 @@ rm $INDX
 echo "# Ukama C4 Diagrams" >> $INDX
 echo "" >> $INDX
 
-
+echo "Processing files in $(realpath ./mermaid)"
 for file in ./mermaid/*.mmd; do
     file_only=${file##*/}
     name=${file_only%.mmd}
     name=${name#structurizr-}
+    echo "Processing $file"
     
     echo  "# ${name}" >> ./md/${name}.md
     echo  "\`\`\`mermaid" >> ./md/${name}.md
