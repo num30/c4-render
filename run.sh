a9930572122243dcc476c8ac90a63e3332f99a23
Denis Palnitsky <palnitsky@gmail.com>
2022-06-19 18:10:43 +0200

    Logging

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-rm -rf md
-mkdir  md
+rm -rf ./md
+mkdir ./md
 
 INDX=README.md
 rm $INDX
@@ -8,22 +8,22 @@ rm $INDX
 echo "# Ukama C4 Diagrams" >> $INDX
 echo "" >> $INDX
 
-echo "Current dir:" $pwd
-echo "Current dirs:"
+echo "Current dir: " $(pwd)
+echo "Current files:"
 ls
 echo List of mermaid files:
-ls mermaid 
-for file in mermaid/*.mmd; do
+ls ./mermaid 
+for file in ./mermaid/*.mmd; do
     file_only=${file##*/}
     name=${file_only%.mmd}
     name=${name#structurizr-}
     echo "Processing $file"
     
-    echo  "# ${name}" >> md/${name}.md
-    echo  "\`\`\`mermaid" >> md/${name}.md
+    echo  "# ${name}" >> ./md/${name}.md
+    echo  "\`\`\`mermaid" >> ./md/${name}.md
     cat $file >> md/${name}.md
     echo "" >> md/${name}.md
-    echo  "\`\`\`" >> md/${name}.md
+    echo  "\`\`\`" >> ./md/${name}.md
     
     # add entry to index 
     echo "- [$name](md/$name.md)" >> $INDX
